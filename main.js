10528c012d42acbb6290b373de2bb524a8164fe0
Mathiew Abbas <mathiew95@gmail.com>
2022-06-23 12:16:55 -0500

    feat: jira ticket appending of body

--- main.js
+++ main.js
@@ -3,16 +3,29 @@ const github = require('@actions/github');
 
 const token = core.getInput('token', { required: true });
 const body = core.getInput('body', { required: true });
+const jiraBaseUrl = core.getInput('body', { required: true });
 
 const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
 
 const prNum = github.context.payload.pull_request.number;
+const branchName = github.context.ref;
 
 const octokit = github.getOctokit(token);
+
+const template = `
+[Jira](${jiraBaseUrl}/${branchName})
+
+---
+
+`;
+
+const finalBody = template.concat(body);
+
+console.log(finalBody);
 
 octokit.pulls.update({
   owner: repoOwner,
   repo: repoName,
-  body: body,
+  body: finalBody,
   pull_number: prNum,
 });
