8c8fcf374621539d0511f443139bd333d8a261a5
Mathiew Abbas <mathiew95@gmail.com>
2022-06-23 19:09:09 -0500

    feat: added ticket number

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  work()
-} catch (error) {
-  core.setFailed(error.message);
-}
 
 async function work() {
-  const token = core.getInput('token', { required: true });
-  const body = core.getInput('body', { required: true });
-  const jiraBaseUrl = core.getInput('jiraBaseUrl', { required: true });
+  try {
+    const token = core.getInput('token', { required: true });
+    const body = core.getInput('body', { required: true });
+    const jiraBaseUrl = core.getInput('jiraBaseUrl', { required: true });
 
-  const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
-  const githubContext = github.context;
-  const githubContextPayload = githubContext.payload;
-  const prNum = githubContextPayload.pull_request.number;
-  const octokit = github.getOctokit(token);
-
-  console.log(githubContext);
-
-  const prInfo = await octokit.rest.pulls.get({
-    owner: repoOwner,
-    repo: repoName,
-    pull_number: prNum,
-  });
-
-  console.log(prInfo);
-
-  const [branchType, ticketNumber] = branchName.split('/');
+    const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/');
+    const githubContext = github.context;
+    const githubContextPayload = githubContext.payload;
+    const prNum = githubContextPayload.pull_request.number;
+    const octokit = github.getOctokit(token);
 
 
-  const template = `
-  [Jira](${jiraBaseUrl}/${ticketNumber})
-  
-  ---
-  
-  `;
+    //To get more details about the PR
+    const prInfo = await octokit.rest.pulls.get({
+      owner: repoOwner,
+      repo: repoName,
+      pull_number: prNum,
+    });
 
-  const finalBody = template.concat(body);
+    const branchName = prInfo.data.head.ref;
 
-  console.log(finalBody);
+    //Split the branch name on the '/' char to get the ticket number
+    const [branchType, ticketNumber] = branchName.split('/');
 
-  octokit.rest.pulls.update({
-    owner: repoOwner,
-    repo: repoName,
-    body: finalBody,
-    pull_number: prNum,
-  });
+    //TODO: add figma link
+    const template = `
+    [Jira](${jiraBaseUrl}/${ticketNumber})
+
+    ---
+    
+    `;
+
+    const finalBody = template.concat(body);
+
+    octokit.rest.pulls.update({
+      owner: repoOwner,
+      repo: repoName,
+      body: finalBody,
+      pull_number: prNum,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+work()
 
 
 
